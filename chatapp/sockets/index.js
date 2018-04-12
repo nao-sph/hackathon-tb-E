aaaa712652005834f3267480ac6e1942d2a2effb
nao-sph <naoki.nagai@icloud.com>
2018-04-12 20:40:11 +0900

    committed in 2018/04/12 20:40:11

--- chatapp/sockets/index.js
+++ chatapp/sockets/index.js
@@ -5,12 +5,14 @@ module.exports = function (server) {
     const socketIo = require('socket.io')(server, { wsEngine: 'ws' });
     const io = socketIo.listen(server);
 
+    const userList = []
+
     io.sockets.on('connection', function (socket) {
         // 投稿モジュールの呼出
         require('./publish')(socket, io);
 
         // 入室モジュールの呼出
-        require('./enter')(socket);
+        require('./enter')(socket, userList);
 
         // 退室モジュールの呼出
         require('./exit')(socket);
