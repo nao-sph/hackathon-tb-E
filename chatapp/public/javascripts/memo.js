1350b90f11148419531910a83d6f13ab4147cce8
nao-sph <naoki.nagai@icloud.com>
2018-04-12 19:33:47 +0900

    committed in 2018/04/12 19:33:47

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,11 +3,11 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').value();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message');
     // メモの内容を表示
     $('#thread').prepend('<p>' + '</p>');
-    
+
     return false;
 }
