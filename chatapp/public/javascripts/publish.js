853b0394864c2ff029f31eb28561e47e90331046
askmusic76 <askmusic76@gmail.com>
2018-04-12 19:35:11 +0900

    committed in 2018/04/12 19:35:11

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -3,15 +3,15 @@
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = '投稿文を入力してください';
     // 投稿内容を送信
-
+    socket.emit('event1',message);
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('event1', function (data) {
+    $('#thread').prepend('<p>'+ data + '</p>');
 });
