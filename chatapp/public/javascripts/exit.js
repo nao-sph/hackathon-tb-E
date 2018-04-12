00720f36c9ec8179f21b559e9878e3a486e07b95
lmk0420 <lmk19960420@gmail.com>
2018-04-12 19:58:41 +0900

    committed in 2018/04/12 19:58:41

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -3,7 +3,7 @@
 // 退室メッセージをサーバに送信する
 function exit() {
     // ユーザ名取得
-    const userName = $('userName').val();
+    const userName = $('#userName').val();
     // 退室メッセージイベントを送信する
     socket.emit('exit', userName);
     // 退室
