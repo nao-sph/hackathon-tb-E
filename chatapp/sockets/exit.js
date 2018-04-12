306deeeafab11b467cb1857b7974ff74dbb15299
lmk0420 <lmk19960420@gmail.com>
2018-04-12 20:02:31 +0900

    committed in 2018/04/12 20:02:31

--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -3,7 +3,7 @@
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
     socket.on('exit', function (data) {
-      data = data + ('さんが退室しました');
+      data = data + ('さんが退室しました。');
       console.log(data);
       socket.broadcast.emit('exit', data);
     });
