73237b1ebd3113468301a212d0c82dcbb775a758
lmk0420 <lmk19960420@gmail.com>
2018-04-13 19:35:55 +0900

    committed in 2018/04/13 19:35:55

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -1,14 +1,17 @@
 'use strict';
+require('date-utils');
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('event1', function (message,userName) {
-      const data = userName + 'さん:' + message;
+      var date = new Date();
+      var formatted = date.toFormat('HH24時MI分')
+
+      const data = '[' + formatted + ']' + userName + 'さん:' + message;
       io.sockets.emit('event1',data);
       console.log(data);
     });
     socket.on('event2', function(data,userName){
       io.sockets.emit('event2',data,userName);
-
     });
 };
