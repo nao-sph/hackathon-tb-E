674dea745fcb790a24ba01f4c97b31c2a4830eb2
lmk0420 <lmk19960420@gmail.com>
2018-04-17 15:44:00 +0900

    committed in 2018/04/17 15:44:00

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,18 +1,8 @@
 'use strict';
 
 // 投稿メッセージをサーバに送信する
-$( '#message' ).keypress( function ( e ) {
-	if ( e.which == 13 ) {
-		// ここに処理を記述
-		if(event.shiftKey){
-			$( '#message' ).val()=$( '#message' ).val()+"\n";
-			return false;
-		}
-        publish();
-		return false;
-	}
-} );
 function publish() {
+
   if (document.getElementById('message').value.replace(/\r?\n/g,"") == "" || document.getElementById('message') == null ) {
       alert('空文字またはnullです');
   } else {
@@ -22,32 +12,21 @@ function publish() {
     const message = $('#message').val();
     // 投稿内容を送信
     socket.emit('event1',message,userName);
-    // io.to(socket.id).emit('event2',data,userName);
+    io.to(socket.id).emit('event2',data,userName);
     $('#message').val('');
     return false;
   }
 }
 
-function dispSelf(formatted,userName,message){
-    var str='<div class="kaiwa"><figure class="kaiwa-img-right"><img src="https://i2.wp.com/sozaikoujou.com/wordpress/wp-content/uploads/2016/06/th_app_icon_account.jpg?w=600&ssl=1" alt="no-img2″><figcaption class="kaiwa-img-description"></figcaption></figure><div class="kaiwa-name-right">'+userName+'</div><div class="kaiwa-text-left"><p class="kaiwa-text">'+message+'</p></div><div class="kaiwa-time-left">'+formatted+'</div></div>';
-    $('#thread').append(str);
-    $('#thread-room').animate({scrollTop: $('#thread-room')[0].scrollHeight}, 'fast');
-}
-function dispOther(formatted,userName,message){
-    var str='<div class="kaiwa"><figure class="kaiwa-img-left"><img src="https://i2.wp.com/sozaikoujou.com/wordpress/wp-content/uploads/2016/06/th_app_icon_account.jpg?w=600&ssl=1" alt="no-img2″><figcaption class="kaiwa-img-description"></figcaption> </figure><div class="kaiwa-name-left">'+userName+'</div><div class="kaiwa-text-right"><p class="kaiwa-text">'+message+'</p></div><div class="kaiwa-time-right">'+formatted+'</div></div>';
-    $('#thread').append(str);
-    $('#thread-room').animate({scrollTop: $('#thread-room')[0].scrollHeight}, 'fast');
-}
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('event1', function (user,formatted,message) {
-	message=message.replace(/\r?\n/g, '<br>');
-    if(userName==user){
-        dispSelf(formatted,user,message);
-    }else{
-        dispOther(formatted,user,message);
-    }
+socket.on('event1', function (data) {
+    $('#thread').prepend('<p>' + data + '</p>');
 });
+
+socket.on('alert', function (data) {
+    alert('前回の投稿から1分以上経っていません');
+});
+
 socket.on('event2', function (data,userName){
-    $('#thread').append('<p>'+ userName + data + '<p>');
-    $('#thread-room').animate({scrollTop: $('#thread-room')[0].scrollHeight}, 'fast');
+    $('#thread').prepend('<p>'+ userName + data + '<p>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -1,16 +1,29 @@
 'use strict';
 require('date-utils');
 
+var postTimeData = new Array();
+
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('event1', function (message,userName) {
+      var event1Frag = 1;
       var date = new Date();
-      // var formatted = date.toFormat('HH24時MI分')
-      var formatted = date.toFormat('HH24:MI')
 
-      const data = formatted + userName + 'さん:' + message;
-      io.sockets.emit('event1',userName,formatted,message);
-      console.log(data);
+      if(userName in postTimeData == false)
+        postTimeData[userName] = date;
+
+      if(userName in postTimeData == true && date.getTime()/1000-postTimeData[userName].getTime()/1000 < 60)
+        event1Frag = 0;
+
+      if (event1Frag = 1){
+        var formatted = date.toFormat('HH24時MI分');
+        const data = '[' + formatted + ']' + userName + 'さん:' + message;
+        io.sockets.emit('event1',data);
+        console.log(data);
+      } else {
+        socket.emit('alert');
+      }
+      console.log(postTimeData);
     });
     socket.on('event2', function(data,userName){
       io.sockets.emit('event2',data,userName);
