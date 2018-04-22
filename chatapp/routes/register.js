99f3c9ab28b012b3d6719317389f3eb3b17f2989
lmk0420 <lmk19960420@gmail.com>
2018-04-22 12:25:43 +0900

    committed in 2018/04/22 12:25:43

---
+++ chatapp/routes/register.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('./db/userData.sqlite3');
+
+router.get('/', function(req, res, next) {
+  res.render('register');
+});
+
+console.log('hello');
+
+router.post('/', function(req, res, next) {
+  var userName = req.body.newUserName;
+  var password = req.body.newPassword;
+
+  console.log(userName);
+
+  //登録
+  db.run('insert into userdata(name,password) values($un, pwd)',
+    {
+      $un:userName,
+      $pwd:password
+    }
+  );
+  db.close();
+  res.redirect('/');
+});
+
+module.exports = router;
